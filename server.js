058b0f7889120d9c2e8adc88c27ded14b80bd07d
Amir Rafique <amique@gmail.com>
2013-08-06 11:06:24 +1000

    added post function

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 // app.listen(process.env.PORT || 5000);
 
+
+app.post('/', function(req, res){
+	res.sendfile('index.html');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
